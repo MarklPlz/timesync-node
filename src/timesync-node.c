937e1f6c3d74c9e8f908c1f2f4c29ec35f1b274b
MarklPlz <165063506+MarklPlz@users.noreply.github.com>
2024-05-24 00:14:46 +0200

    write csv file

--- src/timesync-node.c
+++ src/timesync-node.c
@@ -1,63 +1,79 @@
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 
 #define PORT 12345
 #define BUFFSIZE 1024
 
 int main(void) {
-    int sockfd;
-    struct sockaddr_in addr;
-    socklen_t addr_len = sizeof(addr);
-    char buffer[BUFFSIZE];
+  int sockfd;
+  struct sockaddr_in addr;
+  socklen_t addr_len = sizeof(addr);
+  char buffer[BUFFSIZE];
 
-    // Erstelle ein UDP-Socket
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+  // Erstelle ein UDP-Socket
+  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    perror("socket creation failed");
+    exit(EXIT_FAILURE);
+  }
 
-    // Setze die Socket-Option für Broadcast
-    int broadcastEnable = 1;
-    if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST,
-                   &broadcastEnable, sizeof(broadcastEnable)) < 0) {
-        perror("setsockopt (SO_BROADCAST) failed");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    // Konfiguriere die Adresse
-    memset(&addr, 0, addr_len);
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY); // Empfange von allen Interfaces
-    addr.sin_port = htons(PORT);
-
-    // Binde das Socket an die Adresse
-    if (bind(sockfd, (struct sockaddr *)&addr, addr_len) < 0) {
-        perror("bind failed");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Warte auf Broadcast-Nachrichten auf Port %d...\n", PORT);
-
-    // Empfange Broadcast-Nachrichten
-    while (1) {
-        int recvlen = recvfrom(sockfd, buffer, BUFFSIZE, 0,
-                               (struct sockaddr *)&addr, &addr_len);
-        if (recvlen < 0) {
-            perror("recvfrom failed");
-            close(sockfd);
-            exit(EXIT_FAILURE);
-        }
-        buffer[recvlen] = '\0'; // Null-terminiere den String
-        printf("Empfangen von %s:%d: '%s'\n",
-               inet_ntoa(addr.sin_addr), ntohs(addr.sin_port), buffer);
-    }
-
+  // Setze die Socket-Option für Broadcast
+  int broadcastEnable = 1;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcastEnable,
+                 sizeof(broadcastEnable)) < 0) {
+    perror("setsockopt (SO_BROADCAST) failed");
     close(sockfd);
-    return 0;
-}
+    exit(EXIT_FAILURE);
+  }
 
+  // Konfiguriere die Adresse
+  memset(&addr, 0, addr_len);
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htonl(INADDR_ANY); // Empfange von allen Interfaces
+  addr.sin_port = htons(PORT);
+
+  // Binde das Socket an die Adresse
+  if (bind(sockfd, (struct sockaddr *)&addr, addr_len) < 0) {
+    perror("bind failed");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Warte auf Broadcast-Nachrichten auf Port %d...\n", PORT);
+
+  // Empfange Broadcast-Nachrichten
+  while (1) {
+    int recvlen = recvfrom(sockfd, buffer, BUFFSIZE, 0,
+                           (struct sockaddr *)&addr, &addr_len);
+    if (recvlen < 0) {
+      perror("recvfrom failed");
+      close(sockfd);
+      exit(EXIT_FAILURE);
+    }
+    buffer[recvlen] = '\0'; // Null-terminiere den String
+    printf("Empfangen von %s:%d: '%s'\n", inet_ntoa(addr.sin_addr),
+           ntohs(addr.sin_port), buffer);
+  }
+
+  close(sockfd);
+
+  int length = 5;
+  int timestamps = 1;
+
+  // Open file for writing
+  FILE *csvfile = fopen("./timestamps.csv", "w");
+  if (csvfile == NULL) {
+    printf("writing to file failed\n");
+    return 1;
+  }
+
+  // Write data to the file
+  fprintf(csvfile, "Measured time [us]\n");
+  for (int i = 0; i < length; ++i) {
+    fprintf(csvfile, "%d\n", timestamps);
+  }
+
+  return 0;
+}
